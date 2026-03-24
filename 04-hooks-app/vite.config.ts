c216ce9cb6e1308e37c9ae34f60630508ee9bdf9
Carlos García <gacia.quintupil@gmail.com>
2026-03-24 18:48:54 -0300

    117. TailwindCSS y Estilos

--- 04-hooks-app/vite.config.ts
+++ 04-hooks-app/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite"; //Agregar esta linea
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(), //Agregar esta linea
   ],
-})
+});
